e6d6c462b679d460c4452c2ef0ad5e5ea1a15c42
clemPera <clement.pera@gmail.com>
2025-12-01 18:22:51 +0100

    day1/feat: Finish part two: Add clicks to rotation and start counting them

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -23,8 +23,8 @@ impl From<&str> for Direction {
     }
 }
 
-
 fn main() {
+    let mut all_clicks = 0;
     let mut count = 0;
     let mut dial = START;
 
@@ -35,15 +35,17 @@ fn main() {
         let (direction, rotation) = l.split_at(1);
         println!("dial : {dial:?} / direction : {direction:?} / rotation: {rotation:?}");
 
-        dial = rotate(dial, direction.into(), rotation.parse().unwrap()).unwrap();
+        let (new_dial, clicks) = rotate(dial, direction.into(), rotation.parse().unwrap()).unwrap();
+        dial = new_dial;
 
         if dial == 0 {
             count += 1;
         }
-        
+
+        all_clicks += clicks;
     });
 
-    println!("answer: {count}");
+    println!("\n# Answers:\ncount:  {count}\nclicks: {all_clicks}");
 }
 
 /// Rotate the dial and return the result
@@ -52,82 +54,112 @@ fn main() {
 /// - dial: actual state of the dial before rotation
 /// - direction: which direction to turn the dial
 /// - rotation: number of time to rotate the dial
-fn rotate(mut dial: i16, direction: Direction, rotation: i16) -> Result<i16, i16> {
+/// 
+/// # Returns
+/// Ok(a, b) = a is the new dial, b is the number of clicks
+/// Err(a) = a is the dial number
+fn rotate(dial: i16, direction: Direction, rotation: i16) -> Result<(i16, u16), i16> {
+    let mut new_dial;
+    let mut clicks = 0;
+
     match direction {
         Direction::LEFT => {
-            dial = dial - rotation;
+            new_dial = dial - rotation;
 
-            while dial < MIN {
-                dial = dial + MAX;
+            while new_dial < MIN {
+                new_dial = new_dial + MAX;
+
+                clicks += 1;
             }
-            dial
+
+            //Handle edge cases
+            if new_dial == 0 {
+                clicks += 1
+            }
+
+            if dial == 0 {
+                clicks -= 1;
+            }
         }
         Direction::RIGHT => {
-            dial = dial + rotation;
+            new_dial = dial + rotation;
 
-            while dial >= MAX {
-                dial = dial - MAX;
-            };
-            dial
+            while new_dial >= MAX {
+                new_dial = new_dial - MAX;
+                clicks += 1
+            }
         }
     };
 
-    if dial < MIN || dial > MAX {
-        Err(dial)
+    if new_dial < MIN || new_dial > MAX {
+        Err(new_dial)
     }else{
-        Ok(dial)
+        Ok((new_dial, clicks))
     }
 }
 
 #[test]
 fn rotation_tests(){
     // The dial starts by pointing at 50.
-    // The dial is rotated L68 to point at 82.
+    // The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
     // The dial is rotated L30 to point at 52.
     // The dial is rotated R48 to point at 0.
     // The dial is rotated L5 to point at 95.
-    // The dial is rotated R60 to point at 55.
+    // The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
     // The dial is rotated L55 to point at 0.
     // The dial is rotated L1 to point at 99.
     // The dial is rotated L99 to point at 0.
     // The dial is rotated R14 to point at 14.
-    // The dial is rotated L82 to point at 32.
+    // The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+
 
     let dial = 50;
-    let dial = rotate(dial, Direction::LEFT, 68).unwrap();
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 68).unwrap();
     assert_eq!(dial, 82);
+    assert_eq!(clicks, 1);
     
-    let dial = rotate(dial, Direction::LEFT, 30).unwrap();
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 30).unwrap();
     assert_eq!(dial, 52);
-
-    let dial = rotate(dial, Direction::RIGHT, 48).unwrap();
-    assert_eq!(dial, 0);
-
-    let dial = rotate(dial, Direction::LEFT, 5).unwrap();
-    assert_eq!(dial, 95);
-
-    let dial = rotate(dial, Direction::RIGHT, 60).unwrap();
-    assert_eq!(dial, 55);
-
-    let dial = rotate(dial, Direction::LEFT, 55).unwrap();
-    assert_eq!(dial, 0);
-
-    let dial = rotate(dial, Direction::LEFT, 1).unwrap();
-    assert_eq!(dial, 99);
-
-    let dial = rotate(dial, Direction::LEFT, 99).unwrap();
-    assert_eq!(dial, 0);
+    assert_eq!(clicks, 0);
     
-    let dial = rotate(dial, Direction::RIGHT, 14).unwrap();
+    let (dial, clicks) = rotate(dial, Direction::RIGHT, 48).unwrap();
+    assert_eq!(dial, 0);
+    assert_eq!(clicks, 1);
+    
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 5).unwrap();
+    assert_eq!(dial, 95);
+    assert_eq!(clicks, 0);
+    
+    let (dial, clicks) = rotate(dial, Direction::RIGHT, 60).unwrap();
+    assert_eq!(dial, 55);
+    assert_eq!(clicks, 1);
+    
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 55).unwrap();
+    assert_eq!(dial, 0);
+    assert_eq!(clicks, 1);
+    
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 1).unwrap();
+    assert_eq!(dial, 99);
+    assert_eq!(clicks, 0);
+    
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 99).unwrap();
+    assert_eq!(dial, 0);
+    assert_eq!(clicks, 1);
+    
+    let (dial, clicks) = rotate(dial, Direction::RIGHT, 14).unwrap();
     assert_eq!(dial, 14);
+    assert_eq!(clicks, 0);
 
-    let dial = rotate(dial, Direction::LEFT, 82).unwrap();
+    let (dial, clicks) = rotate(dial, Direction::LEFT, 82).unwrap();
     assert_eq!(dial, 32);
+    assert_eq!(clicks, 1);
 
     //Stress tests: 
-    let dial = rotate(0, Direction::LEFT, 550).unwrap();
+    let (dial, clicks) = rotate(0, Direction::LEFT, 550).unwrap();
     assert_eq!(dial, 50);
+    assert_eq!(clicks, 5);
 
-    let dial = rotate(0, Direction::RIGHT, 550).unwrap();
+    let (dial, clicks) = rotate(0, Direction::RIGHT, 550).unwrap();
     assert_eq!(dial, 50);
+    assert_eq!(clicks, 5);
 }
